d0f57257057c3faaff2285cfc31fafc80f6b6be6
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-10 23:18:03 -0500

    Useful reusable functions

--- Utility.cs
+++ Utility.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +12,45 @@ namespace TiberiTreeGen
     {
         static Random random;
 
+        static float pi = 3.14159265359f;
+
         public Utility(int randomSeed)
         {
-            Utility.random = new Random(randomSeed);
+            //Utility.random = new Random(randomSeed);
+            Utility.random = new Random();
         }
 
         public static int randomInRange(int min, int max)
         {
             return Utility.random.Next(min, max + 1);
+        }
+
+        public static float degreeToRadian(float degree)
+        {
+            return degree * (pi / 180);
+        }
+
+        public static float radianToDegree(float radian)
+        {
+            return (radian * 180) / pi;
+        }
+
+        public static void drawLine(SpriteBatch sb, Texture2D texture, Vector2 start, Vector2 end)
+        {
+            sb.Draw(texture, start, null, Color.White,
+                             (float)Math.Atan2(end.Y - start.Y, end.X - start.X),
+                             new Vector2(0f, (float)texture.Height / 2),
+                             new Vector2(Vector2.Distance(start, end), 3f),
+                             SpriteEffects.None, 0f);
+
+        }
+
+        public static Texture2D createSolidTexture(SpriteBatch sb, Color color)
+        {
+            Texture2D _texture;
+            _texture = new Texture2D(sb.GraphicsDevice, 1, 1);
+            _texture.SetData(new Color[] { color });
+            return _texture;
         }
     }
 }
