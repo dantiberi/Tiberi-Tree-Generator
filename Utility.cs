96b2131c502e21c014d4ad889737bb9a6efe6d36
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-10 20:48:08 -0500

    Created utility class for reusable code.

---
+++ Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiberiTreeGen
+{
+    public class Utility
+    {
+        static Random random;
+
+        public Utility(int randomSeed)
+        {
+            Utility.random = new Random(randomSeed);
+        }
+
+        public static int randomInRange(int min, int max)
+        {
+            return Utility.random.Next(min, max + 1);
+        }
+    }
+}
