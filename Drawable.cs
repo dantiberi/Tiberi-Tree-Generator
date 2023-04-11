cf6c5a9ae9facd2c125691b5126af4178c705514
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-10 23:19:25 -0500

    Update Drawable.cs

--- Drawable.cs
+++ Drawable.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace TiberiTreeGen
         public float xPos { get; set; }
         public float yPos { get; set; }
 
-        public abstract void draw(GameTime gameTime);
+        public abstract void draw(GameTime gameTime, SpriteBatch sb);
         public abstract void update(GameTime gameTime);
         public abstract void load();
     }
