fd4db861ae17db38beeb992147ac0a7d046c1ae7
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-10 20:37:50 -0500

    Created monogame project.

---
+++ Drawable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiberiTreeGen
+{
+    public abstract class Drawable
+    {
+        public float xPos { get; set; }
+        public float yPos { get; set; }
+
+        public abstract void draw(GameTime gameTime);
+        public abstract void update(GameTime gameTime);
+        public abstract void load();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new TiberiTreeGen.Main();
+game.Run();
